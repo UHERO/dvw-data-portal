f797bb55a3608fe1805952739f55ea3c5f21ace2
Victoria Ward <vward@hawaii.edu>
2019-07-03 15:15:59 -1000

    fix print export

--- src/app/module-table/module-table.component.ts
+++ src/app/module-table/module-table.component.ts
@@ -304,33 +304,19 @@ export class ModuleTableComponent implements OnInit, OnChanges {
               fixedColumns.forEach((col, index) => {
                 fixed.push(row[index]);
               });
-              // const indicator = row[0];
-              // const area = row[1];
-              // const units = row[2];
-              // Store source info to append to end of export (include Indicator and Source)
-              // const source = row[row.length - 1];
-              // const sourceCopy = Object.assign({}, source);
-              // let sourceRow = [indicator, sourceCopy];
-              // sourceRow = rowRightPad(sourceRow);
-              // sources.push(sourceRow);
               // Get data from each original row excluding fixed columns and sources
-              const nonFixedCols = row.slice(fixedColumns.length, row.length - 1);
+              const nonFixedCols = row.slice(fixedColumns.length, row.length);
               // Split data into groups of arrays with max length == 7
               const maxLength = fixedColumns.length === 3 ? 5 : 4;
-              const split = splitTable(nonFixedCols, maxLength);
+              const split = splitTable(nonFixedCols, 7);
               for (let i = 0; i < split.length; i++) {
                 // Each group is used as a new row for the formatted tables
                 let newRow = split[i];
                 // Add the fixed columns to each new row
-                // const indicatorCopy = Object.assign({}, indicator);
-                // const areaCopy = Object.assign({}, area);
-                // const unitsCopy = Object.assign({}, units);
                 fixed.forEach((c) => {
                   let cCopy = Object.assign({}, c);
                   newRow.unshift(cCopy);
-                  console.log('newRow', newRow)
                 })
-                // newRow.unshift(indicatorCopy, areaCopy, unitsCopy);
                 if (newRow.length < 10) {
                   newRow = rowRightPad(newRow);
                 }
@@ -346,12 +332,6 @@ export class ModuleTableComponent implements OnInit, OnChanges {
                 }
               }
             });
-            // Add sources
-            /* sources.forEach((source) => {
-              // Right align cell text
-              rightAlign(source);
-              formattedTable.push(source);
-            }); */
             doc.defaultStyle.fontSize = 10;
             doc.styles.tableHeader.fontSize = 10;
             docContent.table.dontBreakRows = true;
@@ -366,6 +346,97 @@ export class ModuleTableComponent implements OnInit, OnChanges {
           text: '<i class="fas fa-print" aria-hidden="true" title="Print"></i>',
           exportOptions: {
             columns: ':visible'
+          },
+          customize: function(win) {
+            function sortObsDates(nonSorted, sorted) {
+              const result = [];
+              for (let i = 0; i < sorted.length; i++) {
+                const index = nonSorted.indexOf(sorted[i]);
+                result[i] = nonSorted[index];
+              }
+              return result;
+            }
+            function splitTable(array, size) {
+              const result = [];
+              for (let i = 0; i < array.length; i += size) {
+                result.push(array.slice(i, i + size));
+              }
+               return result;
+            }
+            // Get array of dates from table
+            const dates = tableColumns.slice(fixedColumns.length, tableColumns.length);
+            const dateArray = [];
+            dates.forEach((date) => {
+              dateArray.push(date.title);
+            });
+            // Get array of columns minus fixed columns
+            const columns = tableColumns.slice(fixedColumns.length);
+            // Split columns into arrays with max length of 7
+            const maxLength = fixedColumns.length === 3 ? 7 : 8;
+            const tableHeaders = splitTable(columns, maxLength);
+            const newTables = [];
+
+            // Add fixed columns to the new table headers and create a new table for each header
+            tableHeaders.forEach((header) => {
+              for (let i = 0; i < fixedColumns.length; i++) {
+                header.unshift(tableColumns[i]);
+              }
+              let html = '<table class="dataTable no-footer"><tr>';
+              header.forEach((col) => {
+                html += '<td>' + col.title + '</td>';
+              });
+              html += '</tr>';
+              newTables.push(html);
+            });
+
+            // Add data from indicators to each new table
+            tableData.series.forEach((ind, index) => {
+              let obsCounter = 0;
+              const observations = Object.keys(ind.observations);
+              // Sort observations keys to match order of table date columns
+              const sortedObs = sortObsDates(observations, dateArray);
+              for (let i = 0; i < newTables.length; i++) {
+                let table = newTables[i];
+                table += '<tr>';
+                fixedColumns.forEach((dim) => {
+                  table += `<td>${ind[dim]}</td>`;
+                });
+                let colCount = fixedColumns.length;
+                while (colCount < 10 && obsCounter < sortedObs.length) {
+                  table += '<td>' + ind.observations[sortedObs[obsCounter]] + '</td>';
+                  colCount += 1;
+                  obsCounter += 1;
+                }
+                if (index === tableData.series.length - 1) {
+                  table += '</table>';
+                }
+                newTables[i] = table;
+              }
+            });
+
+            // Original table
+            const dtTable = $(win.document.body).find('table');
+            newTables.forEach((table) => {
+              $(win.document.body).append('<br>');
+              $(win.document.body).append(table);
+            });
+
+            // Remove original table from print
+            dtTable.remove();
+
+            const $tables = $(win.document.body).find('table');
+            $tables.each(function (i, table) {
+              $(table).find('tr:odd').each(function () {
+                $(this).css('background-color', '#F9F9F9');
+              });
+              $(table).find('td').each(function () {
+                $(this).css('text-align', 'right');
+                $(this).css('width', '10%');
+              });
+            });
+            $(win.document.body)
+              .find('br:last-child')
+              .after('<p>Compiled by Research & Economic Analysis Division, State of Hawaii Department of Business, Economic Development and Tourism. For more information, please visit: http://dbedt.hawaii.gov/economic</p>');
           }
         }
       ]
