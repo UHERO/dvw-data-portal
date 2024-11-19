15e53d1e8ceea4f31fcfc69fb39a5cd24765257d
calebthewood <calebw787@gmail.com>
2024-11-18 18:41:01 -1000

    update landing page and selectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { TourismModuleComponent } from './tourism-module/tourism-module.component';
-import { MatSelectModule } from '@angular/material/select';
-import { DvwApiService } from './dvw-api.service';
-import { HelperService } from './helper.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ModuleListComponent } from './module-list/module-list.component';
 import { DimensionSelectorComponent } from './dimension-selector/dimension-selector.component';
+import { DvwApiService } from './dvw-api.service';
 import { FrequencySelectorComponent } from './frequency-selector/frequency-selector.component';
+import { HelperService } from './helper.service';
+import { ModuleListItemComponent } from './module-list-item/module-list-item.component';
+import { ModuleListComponent } from './module-list/module-list.component';
 import { ModuleTableComponent } from './module-table/module-table.component';
-import { YearSelectorComponent } from './year-selector/year-selector.component';
 import { MonthSelectorComponent } from './month-selector/month-selector.component';
 import { QuarterSelectorComponent } from './quarter-selector/quarter-selector.component';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { TourismModuleComponent } from './tourism-module/tourism-module.component';
+import { YearSelectorComponent } from './year-selector/year-selector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TourismModuleComponent,
     ModuleListComponent,
+    ModuleListItemComponent,
     DimensionSelectorComponent,
     FrequencySelectorComponent,
     ModuleTableComponent,
--- src/app/module-list/module-list.component.ts
+++ src/app/module-list/module-list.component.ts
@@ -1,10 +1,49 @@
+// module-list.component.ts
 import { Component } from '@angular/core';
+
+export interface ModuleItem {
+  icon: string;
+  title: string;
+  description: string;
+  routePath: string;
+}
 
 @Component({
   selector: 'app-module-list',
-  templateUrl: './module-list.component.html',
+  templateUrl:  './module-list.component.html',
   styleUrls: ['./module-list.component.scss']
 })
 export class ModuleListComponent {
-
+  modules: ModuleItem[] = [
+    {
+      icon: 'trending_up',
+      title: 'Visitor Trends',
+      description: 'This section provides data on the number of visitors, days, expenditure (EXPND), length of stay (LOS), daily census, and per person per day spending by market and by island (PPPD, PPPT) for monthly, quarterly, and annually. Data availability varies by indicator, market and island.',
+      routePath: 'trend'
+    },
+    {
+      icon: 'people',
+      title: 'Visitor Characteristics',
+      description: 'Section gives a basic profile of Hawaii\'s visitors. The profile is available by group defined by where they came from, which island they visited, accommodation choice, purpose and type of their visit, and if they\'ve visited Hawaii before.',
+      routePath: 'char'
+    },
+    {
+      icon: 'flight',
+      title: 'Air Seats',
+      description: 'This section shows the count of air seats to Hawaii\'s commercial airports on Oahu, Hawaii Island, Maui, and Kauai. Data are available for monthly, quarterly, and annually for scheduled flights and charter flights by departing region or country.',
+      routePath: 'airseat'
+    },
+    {
+      icon: 'attach_money',
+      title: 'Expenditure Patterns',
+      description: 'This section includes data on spending habits of visitors, either by market or by island visited. Spending for food, entertainment, transportation, shopping, and lodging are shown as daily spending and as a percent of total spending.',
+      routePath: 'exp'
+    },
+    {
+      icon: 'location_city',
+      title: 'Hotel Performance',
+      description: 'This section tracks hotel occupancy, the average daily rate, and revenue per available room. Where available, data can be viewed by island area or hotel type.',
+      routePath: 'hotel'
+    }
+  ];
 }
